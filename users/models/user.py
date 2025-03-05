df009383981ccd6de5e54ee56a7e473bdf61b613
Camilo Acevedo <108965135+camidev234@users.noreply.github.com>
2025-03-05 08:18:46 -0500

    create user model

--- users/models/user.py
+++ users/models/user.py
@@ -1,4 +1,33 @@
 from django.db import models
 
-class User(models.Model):
-    pass
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("El campo de email debe ser proporcionado.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db) 
+        return user
+    
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True, max_length=250)
+    # password = models.CharField(max_length=50)
+    born_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username', 'born_date']
+    
+    objects = UserManager()
+    
+    class Meta:
+        db_table = 'users'
