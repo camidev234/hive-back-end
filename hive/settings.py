ef17fceb186316a30ff962db7602b0925dd40221
Camilo Acevedo <108965135+camidev234@users.noreply.github.com>
2025-03-05 11:24:35 -0500

    add cors

--- hive/settings.py
+++ hive/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'users'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hive.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
 
 TEMPLATES = [
     {
